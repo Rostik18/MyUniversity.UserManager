0b55974f316d790bd1539dde2f221dc81911f017
Rostik18 <rostik1804@gmail.com>
2021-02-14 14:00:06 +0200

    Remove RabbitMQ and add host with startup file

---
+++ src/MyUniversity.UserManager.Api/Extensions/HostExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyUniversity.UserManager.Api.Extensions
+{
+    public static class HostExtensions
+    {
+        public static void AddCustomConfigurations(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddSingleton(configuration.GetSection(nameof(MessageClientConfigs)).Get<MessageClientConfigs>());
+        }
+
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            //services.AddSingleton<IMessageClient, MessageClient>();
+
+        }
+    }
+}
---
+++ src/MyUniversity.UserManager.Api/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyUniversity.UserManager.Api.Extensions;
+
+namespace MyUniversity.UserManager.Api
+{
+    class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCustomConfigurations(Configuration);
+            services.AddCustomServices();
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+        }
+    }
+}

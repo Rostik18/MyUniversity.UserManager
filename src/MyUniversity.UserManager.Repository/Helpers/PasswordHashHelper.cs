bb81db1b3ee77358d4b32d7cb065892afb0871af
RostyslavBaitsar <rostik1804@gmail.com>
2021-03-25 14:51:30 +0200

    Clean up and recompile

--- src/MyUniversity.UserManager.Repository/Helpers/PasswordHashHelper.cs
+++ src/MyUniversity.UserManager.Repository/Helpers/PasswordHashHelper.cs
@@ -1,24 +1,24 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
+using Grpc.Core;
 
 namespace MyUniversity.UserManager.Repository.Helpers
 {
     public static class PasswordHashHelper
     {
-        private const int _expectedHashLength = 64;
-        private const int _expectedSaltLength = 128;
+        private const int ExpectedHashLength = 64;
+        private const int ExpectedSaltLength = 128;
 
         public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
             if (string.IsNullOrWhiteSpace(password))
             {
-                throw new BadArgumentException("Password cannot be null or whitespace.");
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Password cannot be null or whitespace"));
             }
 
-            using (var hmac = new System.Security.Cryptography.HMACSHA512())
-            {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-            }
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
         }
 
         public static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
@@ -26,33 +26,23 @@ namespace MyUniversity.UserManager.Repository.Helpers
 
             if (string.IsNullOrWhiteSpace(password))
             {
-                throw new BadArgumentException("Password cannot be null or whitespace.");
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Password cannot be null or whitespace"));
             }
 
-            if (storedHash.Length != _expectedHashLength)
+            if (storedHash.Length != ExpectedHashLength)
             {
-                throw new BadArgumentException("Invalid lenght of password hash.");
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid length of password hash"));
             }
 
-            if (storedSalt.Length != _expectedSaltLength)
+            if (storedSalt.Length != ExpectedSaltLength)
             {
-                throw new BadArgumentException("Invalid lenght of password sailt.");
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid length of password salt"));
             }
 
-            using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
-            {
-                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            using var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
 
-                for (int i = 0; i < computedHash.Length; i++)
-                {
-                    if (computedHash[i] != storedHash[i])
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
+            return !computedHash.Where((t, i) => t != storedHash[i]).Any();
         }
     }
 }
